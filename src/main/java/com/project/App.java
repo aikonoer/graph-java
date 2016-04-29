579bf4d22fef2cce100b762105c4cf9d7adbdc14
aikonoer <brianmomongan@live.com.au>
2016-04-30 00:54:26 +1000

    text processing

--- src/main/java/com/project/App.java
+++ src/main/java/com/project/App.java
@@ -2,7 +2,13 @@ package com.project;
 
 import com.project.models.DiGraph;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Hello world!
@@ -10,15 +16,48 @@ import java.util.*;
 public class App {
     public static void main(String[] args) {
 
-        DiGraph<String> graph = new DiGraph<>();
-        String graphRoutes = "AB20 AD80 AG90 BF10 CF50 CD10 CH20 DC10 DG20 EG30 EB50 FC10 FD40 GA20";
-        graph.createGraph(graphRoutes);
-        Integer distance = graph.findDistance("ABFCDCDG", 7);
-        String shortest = graph.dijkstraShortestPath("A", "G", new HashMap<>(), new HashSet<>(), new LinkedList<>());
-        Set<String> possibleRoutes = graph.possibleRoutes("A", "G", 100, new HashSet<>());
-        System.out.println("Distance: " + distance);
-        System.out.println("Shortest: " + shortest);
-        System.out.println("Possible: " + possibleRoutes);
-        System.out.println();
+        String inputString = "/home/brianmomongan/Documents/input.txt";
+        final Path path = Paths.get(inputString);
+        List<String> queue = new LinkedList<>();
+        DiGraph<String> graph;
+
+        try (Stream<String> stream = Files.lines(path)) {
+            stream.forEach(queue::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        if (!queue.isEmpty()) {
+            graph = new DiGraph<>();
+            graph.createGraph(queue.get(0));
+            final String distance = "DISTANCE";
+            final String shortest = "SHORTEST";
+            final String possible = "POSSIBLE";
+            List<String> results = queue
+                    .stream()
+                    .map(lines -> {
+                        if (lines.startsWith(distance)) {
+                            String input = lines.substring(distance.length()).trim();
+                            return distance + " " + input + " = " + input + graph.findDistance(input, input.length() - 1);
+                        } else if (lines.startsWith(shortest)) {
+                            String input = lines.substring(shortest.length()).trim();
+                            return shortest + " " + input + " = " + graph.dijkstraShortestPath(input.substring(0, 1), input.substring(1, 2), new HashMap<>(), new HashSet<>(), new LinkedList<>());
+                        } else if (lines.startsWith(possible)) {
+                            String input = lines.substring(shortest.length()).trim();
+                            StringBuilder builder = new StringBuilder();
+                            Set<String> result = graph.possibleRoutes(input.substring(0, 1), input.substring(1, 2), Integer.parseInt(input.substring(2)), new HashSet<>());
+                            for (String s : result) {
+                                builder.append(s).append(" ");
+                            }
+                            return input + " = " + builder.toString().trim();
+                        } else {
+                            return null;
+                        }
+                    }).collect(Collectors.toList());
+            System.out.println(results);
+        } else {
+            System.out.println("Graph routes not found");
+        }
     }
 }
